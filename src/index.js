4189d13413d073c0dc068ada608d13d60e96b6b2
Sindre Bøyum <sindre.boyum@tietoevry.com>
2023-06-16 14:52:13 +0200

    build: update deps

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-import { StrictMode } from "react";
-import ReactDOM from "react-dom";
+// @ts-check
 
-import App from "./App";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(
+import App from './App';
+
+const rootElement = document.getElementById('root');
+if (!rootElement) {
+  throw new Error('Root element not found');
+}
+
+createRoot(rootElement).render(
   <StrictMode>
     <App />
   </StrictMode>,
-  rootElement
 );
